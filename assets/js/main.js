a2fee799dcd7a15f701960d33e4589ed1aa97b0a
karfaisarra <karfaisarra@gmail.com>
2022-10-12 18:08:56 +0200

    add carousel with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 const slides = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
 
 //selezione l'elemento della DOM
-const slidesEl = document.querySelector('.my_slides');
+const slidesEl = document.querySelector('.slides');
 
 //creo variabile per vedere la slide attiva
 let imageActive = 0;
@@ -20,10 +20,27 @@ const prevEl = document.querySelector('.prev');
 //click sul pulsante next
 nextEl.addEventListener('click', function(){
     console.log('hai cliccato next');
-
+    //selezione immagini
+    const allSlides = document.querySelectorAll('.slides > img');
+    const currentSlide = allSlides[imageActive];
+    //tolgo la classe active
+    currentSlide.classList.remove('active');
+    imageActive++;
+    const nextSlide = allSlides[imageActive]
+    //aggiungo la class active alla seconda 
+    nextSlide.classList.add('active');
 })
 
 //click sul pulsante prev
 prevEl.addEventListener('click', function(){
     console.log('hai cliccato prev');
+    //selezione immagini
+    const allSlides = document.querySelectorAll('.slides > img');
+    const currentSlide = allSlides[imageActive];
+    //tolgo la classe active
+    currentSlide.classList.remove('active');
+    imageActive--;
+    const nextSlide = allSlides[imageActive]
+    //aggiungo la class active alla seconda 
+    nextSlide.classList.add('active');
 })
